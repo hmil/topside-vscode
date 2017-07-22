b760274b818f53545e0ff060bf5c0ee5a780fdcc
Hadrien Milano <hadrien.milano@gmail.com>
2017-07-22 12:27:06 +0200

    typos

--- src/extension.ts
+++ src/extension.ts
@@ -57,14 +57,14 @@ export function activate(context: vscode.ExtensionContext) {
         documentSelector: ['topside'],
         synchronize: {
             // Synchronize the setting section 'languageServerExample' to the server
-            configurationSection: 'languageServerExample',
+            // configurationSection: 'languageServerExample',
             // Notify the server about file changes to '.clientrc files contain in the workspace
             fileEvents: vscode.workspace.createFileSystemWatcher('**/.clientrc')
         }
     }
 
     // Create the language client and start the client.
-    let disposable = new LanguageClient('languageServerExample', 'Language Server Example', serverOptions, clientOptions).start();
+    let disposable = new LanguageClient('topsideLanguageServer', 'Topside Language Server', serverOptions, clientOptions).start();
 
     // Push the disposable to the context's subscriptions so that the
     // client can be deactivated on extension deactivation
